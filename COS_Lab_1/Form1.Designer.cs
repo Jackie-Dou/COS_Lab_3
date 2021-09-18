4e141855c25fc7cad98dca08b35f9db8e8a77a9e
Jackie-Dou <kasach1477@mail.ru>
2021-09-18 16:43:23 +0300

    Основа гармонического сигнала

--- COS_Lab_1/Form1.Designer.cs
+++ COS_Lab_1/Form1.Designer.cs
@@ -30,10 +30,10 @@ namespace COS_Lab_1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.signalChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.txtSwing = new System.Windows.Forms.TextBox();
             this.txtFrequency = new System.Windows.Forms.TextBox();
             this.txtPhase = new System.Windows.Forms.TextBox();
@@ -45,52 +45,56 @@ namespace COS_Lab_1
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.labelError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.signalChart)).BeginInit();
             this.SuspendLayout();
             // 
-            // chart1
+            // signalChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(263, 12);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(510, 426);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            chartArea1.Name = "ChartArea1";
+            this.signalChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.signalChart.Legends.Add(legend1);
+            this.signalChart.Location = new System.Drawing.Point(286, 12);
+            this.signalChart.Name = "signalChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.signalChart.Series.Add(series1);
+            this.signalChart.Size = new System.Drawing.Size(1294, 430);
+            this.signalChart.TabIndex = 0;
+            this.signalChart.Text = "chart1";
             // 
             // txtSwing
             // 
             this.txtSwing.Location = new System.Drawing.Point(13, 29);
             this.txtSwing.Name = "txtSwing";
-            this.txtSwing.Size = new System.Drawing.Size(100, 22);
+            this.txtSwing.Size = new System.Drawing.Size(121, 22);
             this.txtSwing.TabIndex = 1;
+            this.txtSwing.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSwing_KeyPress);
             // 
             // txtFrequency
             // 
             this.txtFrequency.Location = new System.Drawing.Point(13, 84);
             this.txtFrequency.Name = "txtFrequency";
-            this.txtFrequency.Size = new System.Drawing.Size(100, 22);
+            this.txtFrequency.Size = new System.Drawing.Size(121, 22);
             this.txtFrequency.TabIndex = 2;
+            this.txtFrequency.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFrequency_KeyPress);
             // 
             // txtPhase
             // 
             this.txtPhase.Location = new System.Drawing.Point(13, 137);
             this.txtPhase.Name = "txtPhase";
-            this.txtPhase.Size = new System.Drawing.Size(100, 22);
+            this.txtPhase.Size = new System.Drawing.Size(121, 22);
             this.txtPhase.TabIndex = 3;
+            this.txtPhase.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhase_KeyPress);
             // 
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1592, 24);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -102,6 +106,7 @@ namespace COS_Lab_1
             // 
             // cbbxN
             // 
+            this.cbbxN.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbbxN.FormattingEnabled = true;
             this.cbbxN.Location = new System.Drawing.Point(13, 189);
             this.cbbxN.Name = "cbbxN";
@@ -113,9 +118,9 @@ namespace COS_Lab_1
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(16, 64);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(191, 17);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Введите f (частота)";
+            this.label2.Text = "Введите f (частота) (герцы)";
             // 
             // label1
             // 
@@ -131,9 +136,9 @@ namespace COS_Lab_1
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(13, 113);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(199, 17);
+            this.label3.Size = new System.Drawing.Size(267, 17);
             this.label3.TabIndex = 10;
-            this.label3.Text = "Введите φ (начальная фаза)";
+            this.label3.Text = "Введите φ (начальная фаза)(радианы)";
             // 
             // label4
             // 
@@ -148,17 +153,26 @@ namespace COS_Lab_1
             // 
             this.btnStart.Location = new System.Drawing.Point(13, 398);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(121, 29);
             this.btnStart.TabIndex = 12;
             this.btnStart.Text = "Сделать хорошо";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(13, 241);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 17);
+            this.labelError.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1592, 454);
+            this.Controls.Add(this.labelError);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -168,12 +182,13 @@ namespace COS_Lab_1
             this.Controls.Add(this.txtPhase);
             this.Controls.Add(this.txtFrequency);
             this.Controls.Add(this.txtSwing);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.signalChart);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.signalChart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,7 +196,7 @@ namespace COS_Lab_1
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart signalChart;
         private System.Windows.Forms.TextBox txtSwing;
         private System.Windows.Forms.TextBox txtFrequency;
         private System.Windows.Forms.TextBox txtPhase;
@@ -193,6 +208,7 @@ namespace COS_Lab_1
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label labelError;
     }
 }
 
--- COS_Lab_1/Form1.cs
+++ COS_Lab_1/Form1.cs
@@ -17,18 +17,95 @@ namespace COS_Lab_1
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbbxN.Items.Add("64");
+            cbbxN.Items.Add("128");
+            cbbxN.Items.Add("256");
+            cbbxN.Items.Add("512");
+            cbbxN.Items.Add("1024");
+            cbbxN.Items.Add("2048");
+            signalChart.Series[0]["PixelPointWidth"] = "1";
+
+        }
+
+        private double[] GetChartResults(int swing, int frequency, int phase, int N)
+        {
+            double[] results = new double[N];
+            for (int n = 0; n < N; n++)
+            {
+                // TODO переписать с синуса на ряды Тейлора - или что там Дементору надо
+                double value = swing * Math.Sin((2 * Math.PI * frequency * n) / N + phase);
+                results[n] = value;
+            }
+            return results;
+        }
+
+        private void ShowChart(double[] ordinates, int N)
+        {
+            for (int n = 0; n < N; n++)
+            {
+                signalChart.Series[0].Points.AddXY(n, ordinates[n]);
+            }
+            return;
+        }
+
         private void btnStart_Click(object sender, EventArgs e)
         {
-            int swing = Int32.Parse(txtSwing.Text);
-            int frequency = Int32.Parse(txtFrequency.Text);
-            int phase = Int32.Parse(txtPhase.Text);
-            int N = Int32.Parse(cbbxN.Text);
+            // TODO где-то на форме будет менюшка выбора типа сигнала, выбирать путь исполнения
+            signalChart.Series[0].Points.Clear();
 
-            // TO DO собрать в формулу
-            // вывести на график
-            // наслаждаться
+            int swing, frequency, phase, N;
+            try
+            {
+                swing = Int32.Parse(txtSwing.Text);
+                frequency = Int32.Parse(txtFrequency.Text);
+                phase = Int32.Parse(txtPhase.Text);
+                N = Int32.Parse(cbbxN.Text);
+
+                // TODO добавить ограничения ввода, связанные с внутренней логикой - типа теоремы Котельникова
+
+            } catch
+            {
+                labelError.Text = "Введи нормально, угашенный ты об дерево";
+                return;
+            }
+            labelError.Text = "Молодец, можешь, когда хочешь";
+
+            double[] ordinates = new double[N];
+            ordinates = GetChartResults(swing, frequency, phase, N);
+
+            ShowChart(ordinates, N);
+            return;
+        }
 
 
+        // настройки ограничений ввода
+        private void txtSwing_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtFrequency_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPhase_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != '-')
+            {
+                e.Handled = true;
+            }
         }
     }
 }
