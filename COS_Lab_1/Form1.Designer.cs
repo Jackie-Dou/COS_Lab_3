a365c34e35ec05ceec7ef767d7572b63c47f4b2b
Jackie-Dou <kasach1477@mail.ru>
2021-10-13 18:18:20 +0300

    Переписать интерфейс

--- COS_Lab_1/Form1.Designer.cs
+++ COS_Lab_1/Form1.Designer.cs
@@ -30,9 +30,15 @@ namespace COS_Lab_1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.signalChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.txtSwing = new System.Windows.Forms.TextBox();
             this.txtFrequency = new System.Windows.Forms.TextBox();
@@ -46,30 +52,37 @@ namespace COS_Lab_1
             this.label4 = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
             this.labelError = new System.Windows.Forms.Label();
-            this.cbbxSignal = new System.Windows.Forms.ComboBox();
+            this.chartSwing = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartPhase = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.signalChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSwing)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPhase)).BeginInit();
             this.SuspendLayout();
             // 
             // signalChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.signalChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.signalChart.Legends.Add(legend2);
-            this.signalChart.Location = new System.Drawing.Point(286, 12);
+            this.signalChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.signalChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.signalChart.Legends.Add(legend1);
+            this.signalChart.Location = new System.Drawing.Point(12, 73);
             this.signalChart.Name = "signalChart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.signalChart.Series.Add(series2);
-            this.signalChart.Size = new System.Drawing.Size(1294, 430);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.signalChart.Series.Add(series1);
+            this.signalChart.Size = new System.Drawing.Size(1249, 284);
             this.signalChart.TabIndex = 0;
             this.signalChart.Text = "chart1";
             // 
             // txtSwing
             // 
-            this.txtSwing.Location = new System.Drawing.Point(13, 118);
+            this.txtSwing.Location = new System.Drawing.Point(16, 32);
             this.txtSwing.Name = "txtSwing";
             this.txtSwing.Size = new System.Drawing.Size(121, 22);
             this.txtSwing.TabIndex = 1;
@@ -77,7 +90,7 @@ namespace COS_Lab_1
             // 
             // txtFrequency
             // 
-            this.txtFrequency.Location = new System.Drawing.Point(13, 177);
+            this.txtFrequency.Location = new System.Drawing.Point(205, 32);
             this.txtFrequency.Name = "txtFrequency";
             this.txtFrequency.Size = new System.Drawing.Size(121, 22);
             this.txtFrequency.TabIndex = 2;
@@ -85,7 +98,7 @@ namespace COS_Lab_1
             // 
             // txtPhase
             // 
-            this.txtPhase.Location = new System.Drawing.Point(13, 236);
+            this.txtPhase.Location = new System.Drawing.Point(413, 32);
             this.txtPhase.Name = "txtPhase";
             this.txtPhase.Size = new System.Drawing.Size(121, 22);
             this.txtPhase.TabIndex = 3;
@@ -96,7 +109,7 @@ namespace COS_Lab_1
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1592, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1273, 24);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -110,7 +123,7 @@ namespace COS_Lab_1
             // 
             this.cbbxN.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbbxN.FormattingEnabled = true;
-            this.cbbxN.Location = new System.Drawing.Point(13, 294);
+            this.cbbxN.Location = new System.Drawing.Point(703, 32);
             this.cbbxN.Name = "cbbxN";
             this.cbbxN.Size = new System.Drawing.Size(121, 24);
             this.cbbxN.TabIndex = 6;
@@ -118,7 +131,7 @@ namespace COS_Lab_1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 157);
+            this.label2.Location = new System.Drawing.Point(202, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(191, 17);
             this.label2.TabIndex = 8;
@@ -127,7 +140,7 @@ namespace COS_Lab_1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 98);
+            this.label1.Location = new System.Drawing.Point(13, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(161, 17);
             this.label1.TabIndex = 9;
@@ -136,7 +149,7 @@ namespace COS_Lab_1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 216);
+            this.label3.Location = new System.Drawing.Point(410, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(267, 17);
             this.label3.TabIndex = 10;
@@ -145,7 +158,7 @@ namespace COS_Lab_1
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 274);
+            this.label4.Location = new System.Drawing.Point(700, 7);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(220, 17);
             this.label4.TabIndex = 11;
@@ -153,7 +166,7 @@ namespace COS_Lab_1
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(13, 398);
+            this.btnStart.Location = new System.Drawing.Point(926, 25);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(121, 29);
             this.btnStart.TabIndex = 12;
@@ -169,32 +182,71 @@ namespace COS_Lab_1
             this.labelError.Size = new System.Drawing.Size(0, 17);
             this.labelError.TabIndex = 13;
             // 
-            // cbbxSignal
+            // chartSwing
             // 
-            this.cbbxSignal.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbbxSignal.FormattingEnabled = true;
-            this.cbbxSignal.Location = new System.Drawing.Point(13, 28);
-            this.cbbxSignal.Name = "cbbxSignal";
-            this.cbbxSignal.Size = new System.Drawing.Size(180, 24);
-            this.cbbxSignal.TabIndex = 14;
+            this.chartSwing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chartSwing.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartSwing.Legends.Add(legend2);
+            this.chartSwing.Location = new System.Drawing.Point(12, 401);
+            this.chartSwing.Name = "chartSwing";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartSwing.Series.Add(series2);
+            this.chartSwing.Size = new System.Drawing.Size(604, 300);
+            this.chartSwing.TabIndex = 14;
+            this.chartSwing.Text = "chart1";
+            // 
+            // chartPhase
+            // 
+            this.chartPhase.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.Name = "ChartArea1";
+            this.chartPhase.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartPhase.Legends.Add(legend3);
+            this.chartPhase.Location = new System.Drawing.Point(636, 401);
+            this.chartPhase.Name = "chartPhase";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chartPhase.Series.Add(series3);
+            this.chartPhase.Size = new System.Drawing.Size(625, 300);
+            this.chartPhase.TabIndex = 15;
+            this.chartPhase.Text = "chart2";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(16, 5);
+            this.label5.Location = new System.Drawing.Point(12, 380);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(177, 17);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "Выберите тип сигнала";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Амплитуда";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(633, 380);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Фаза";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1592, 454);
+            this.ClientSize = new System.Drawing.Size(1273, 713);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.cbbxSignal);
+            this.Controls.Add(this.chartPhase);
+            this.Controls.Add(this.chartSwing);
             this.Controls.Add(this.labelError);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label4);
@@ -207,11 +259,15 @@ namespace COS_Lab_1
             this.Controls.Add(this.txtSwing);
             this.Controls.Add(this.signalChart);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.signalChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSwing)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPhase)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,8 +288,10 @@ namespace COS_Lab_1
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label labelError;
-        private System.Windows.Forms.ComboBox cbbxSignal;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSwing;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPhase;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- COS_Lab_1/Form1.cs
+++ COS_Lab_1/Form1.cs
@@ -19,136 +19,43 @@ namespace COS_Lab_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            signalChart.Series.Add("1");
             cbbxN.Items.Add("64");
             cbbxN.Items.Add("128");
             cbbxN.Items.Add("256");
             cbbxN.Items.Add("512");
             cbbxN.Items.Add("1024");
             cbbxN.Items.Add("2048");
-            cbbxSignal.Items.Add("Гармонический");
-            cbbxSignal.Items.Add("Полигармонический");
-            cbbxSignal.Items.Add("Прямоугольный");
-            cbbxSignal.Items.Add("Треугольный");
-            cbbxSignal.Items.Add("Пилообразный");
-            signalChart.Series[0]["PixelPointWidth"] = "1";
+            signalChart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            signalChart.Series[0].Color = Color.Red;
+            signalChart.Series["1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            //signalChart.Series[0]["PixelPointWidth"] = "1";
 
         }
 
-        private double[] GetHarmonic(int swing, int frequency, double phase, int N)
+/*        private double[] GetHarmonic(int swing, int frequency, double phase, int N)
         {
             double[] results = new double[N];
             for (int n = 0; n < N; n++)
             {
-                double value = swing * sinTeylor((2 * Math.PI * frequency * n) / N + phase%(2*Math.PI));
+                double value = swing * sinTeylor((2 * Math.PI * frequency * n) / N + phase);
+                //double value = swing * Math.Sin((2 * Math.PI * frequency * n) / N + phase);
                 results[n] = value;
             }
             return results;
-        }
+        }*/
 
         private double[] GetPolyharmonic(string[] swings, string[] frequences, string[] phases, int N)
         {
             double[] results = new double[N];
             for (int n = 0; n < N; n++)
             {
-                double value = Int32.Parse(swings[0]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[0]) * n) / N + Double.Parse(phases[0]));
-                value += Int32.Parse(swings[1]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[1]) * n) / N + Double.Parse(phases[1]));
-                value += Int32.Parse(swings[2]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[2]) * n) / N + Double.Parse(phases[2]));
-                value += Int32.Parse(swings[3]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[3]) * n) / N + Double.Parse(phases[3]));
+                double value = Int32.Parse(swings[0]) * sinTeylor((2 * Math.PI * Int32.Parse(frequences[0]) * n) / N + Double.Parse(phases[0]));
+                value += Int32.Parse(swings[1]) * sinTeylor((2 * Math.PI * Int32.Parse(frequences[1]) * n) / N + Double.Parse(phases[1]));
+                value += Int32.Parse(swings[2]) * sinTeylor((2 * Math.PI * Int32.Parse(frequences[2]) * n) / N + Double.Parse(phases[2]));
                 results[n] = value;
             }
             return results;
-        }
-
-        private double?[] GetPolyharmonicTable(string[] swings, string[] frequences, string[] phases, int N)
-        {
-            int limitN = N / NOD(ParceArrToInt(frequences));
-            double?[] results = new double?[limitN];
-            for (int n = 0; n < N; n++)
-            {
-                int newN = n % limitN;
-                if (results[newN] == null)
-                {
-                    double value = Int32.Parse(swings[0]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[0]) * newN) / N + Double.Parse(phases[0]));
-                    value += Int32.Parse(swings[1]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[1]) * newN) / N + Double.Parse(phases[1]));
-                    value += Int32.Parse(swings[2]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[2]) * newN) / N + Double.Parse(phases[2]));
-                    value += Int32.Parse(swings[3]) * Math.Sin((2 * Math.PI * Int32.Parse(frequences[3]) * newN) / N + Double.Parse(phases[3]));
-                    results[newN] = value;
-                }
-            }
-            return results;
-        }
-
-        private int[] ParceArrToInt(string[] arr)
-        {
-            int[] result = new int[arr.Count()];
-            for(int i=0; i<arr.Count(); i++)
-            {
-                result[i] = Int32.Parse(arr[i]);
-            }
-            return result;
-        }
-        private double[] GetRectangular(int swing, int frequency, double phase, int N)
-        {
-            double[] results = new double[N];
-            for (int n = 0; n < N; n++)
-            {
-                double value = swing * CountRectangular((2 * Math.PI * frequency * n) / N + phase); 
-                results[n] = value;
-            }
-            return results;
-        }
-
-        private double CountRectangular(double x)
-        {
-            if (sinTeylor(x)<0)
-                return -1;
-            else
-                return 1;
-        }
-
-        private double[] GetTriangular(int swing, int frequency, double phase, int N)
-        {
-            double[] results = new double[N];   
-            int count = N / frequency;
-            double phaseCount = (phase + Math.PI/2) * N / (4 * Math.PI);
-            for (int n = 0; n < N; n++)
-            {
-                double value = 2 * swing * CountTriangular((n + phaseCount) % count, count) - swing;
-                results[n] = value;
-            }
-            return results;
-        }
-        private double CountTriangular(double n, double maxN)
-        {
-            if (n < maxN / 2)
-                return n * 2 / maxN;
-            else
-                return 2 - (2*n / maxN);
-        }
-
-        private double[] GetSawtooth(int swing, int frequency, double phase, int N)
-        {
-            double[] results = new double[N];
-            for (int n = 0; n < N; n++)
-            {
-                double value = 2*swing * CountSawtoon(2 * Math.PI * frequency * ((n + ((phase + Math.PI) * N / (4 * Math.PI) )) % (N/frequency)) / N)/(13.2873/2) - swing;
-                results[n] = value;
-            }
-            return results;
-        }
-
-        private double CountSawtoon(double x, double eps = 0.001)
-        {
-            int i = 1;
-            double temp = sinTeylor(i)*x/i;
-            double sum = 0;
-            while (Math.Abs(temp) > eps)
-            {
-                sum += temp;
-                i++;
-                temp = sinTeylor(i)*x / i;
-            }
-            return sum;
         }
 
 
@@ -157,6 +64,7 @@ namespace COS_Lab_1
             for (int n = 0; n < N; n++)
             {
                 signalChart.Series[0].Points.AddXY(n, ordinates[n]);
+                signalChart.Series["1"].Points.AddXY(n, ordinates[n]);
             }
             return;
         }
@@ -164,29 +72,13 @@ namespace COS_Lab_1
         private void btnStart_Click(object sender, EventArgs e)
         {
             signalChart.Series[0].Points.Clear();
+            signalChart.Series["1"].Points.Clear();
 
-            int swing = 0, frequency = 0, N = 0;
+            int N = 0;
             string[] swings = { }, frequences = { }, phases = { };
-            double phase = 0;
-            string type;
 
             try
             {
-                type = cbbxSignal.Text;
-                if (type != "Полигармонический")
-                {
-                    swing = Int32.Parse(txtSwing.Text);
-                    frequency = Int32.Parse(txtFrequency.Text);
-                    phase = Double.Parse(txtPhase.Text);
-                    N = Int32.Parse(cbbxN.Text);
-
-                    if (frequency < 1 || N <= 2 * frequency)
-                    {
-                        throw new Exception("Логические ограничения");
-                    }
-                }
-                else
-                {
                     string swingsText = txtSwing.Text;
                     swings = swingsText.Split(' ');
                     string frequencesText = txtFrequency.Text;
@@ -195,7 +87,7 @@ namespace COS_Lab_1
                     phases = phasesText.Split(' ');
                     N = Int32.Parse(cbbxN.Text);
 
-                    if (swings.Count() != 4 || frequences.Count() != 4 || phases.Count() != 4)
+                    if (swings.Count() != 3 || frequences.Count() != 3 || phases.Count() != 3)
                     {
                         throw new Exception("Не весь ввод");
                     }
@@ -204,16 +96,13 @@ namespace COS_Lab_1
                         Int32.Parse(frequences[0]) < 1 ||
                         Int32.Parse(frequences[1]) < 1 ||
                         Int32.Parse(frequences[2]) < 1 ||
-                        Int32.Parse(frequences[3]) < 1 ||
                         N <= 2 * Int32.Parse(frequences[0]) ||
                         N <= 2 * Int32.Parse(frequences[1]) ||
-                        N <= 2 * Int32.Parse(frequences[2]) ||
-                        N <= 2 * Int32.Parse(frequences[3])
+                        N <= 2 * Int32.Parse(frequences[2]) 
                         )
                     {
                         throw new Exception("Логические ограничения");
-                    }
-                }
+                    }              
             } catch
             {
                 MessageBox.Show(
@@ -224,59 +113,29 @@ namespace COS_Lab_1
 
             double[] ordinates = new double[N];
 
-            switch (type)
-            {
-                case "Полигармонический":
-                    ordinates = GetPolyharmonic(swings, frequences, phases, N);
-                    break;
-                case "Гармонический":
-                    ordinates = GetHarmonic(swing, frequency, phase, N);
-                    break;
-                case "Прямоугольный":
-                    ordinates = GetRectangular(swing, frequency, phase, N);
-                    break;
-                case "Треугольный":
-                    ordinates = GetTriangular(swing, frequency, phase, N);
-                    break;
-                case "Пилообразный":
-                    ordinates = GetSawtooth(swing, frequency, phase, N);
-                    break;
-            }
+
+            ordinates = GetPolyharmonic(swings, frequences, phases, N);
+
 
             ShowChart(ordinates, N);
             return;
         }
 
-        private double sinTeylor(double x)
+        private double sinTeylor(double x, double eps = 0.0001)
         {
             double taylor = 0;
             int tempFact = 1;
             double piNum = Math.Floor(x / (2 * Math.PI));
             double newX = x - (2 * piNum * Math.PI);
             double tempX = newX;
-            while ((tempFact+1)/2<=4)
+            while (Math.Abs(tempX)>eps)
             {
                 taylor += tempX;
-                tempX = tempX * Math.Pow(newX, 2) * (-1);
+                double del = (-1) * (tempFact + 1) * (tempFact + 2);
+                tempX = tempX * Math.Pow(newX, 2)/del;
                 tempFact += 2;
             }
             return taylor;
-        }
-
-        private int NOD(int[] arr)
-        {
-            int x = Math.Min(arr[0], arr[1]);
-            for (int i=2; i<arr.Count(); i++)
-            {
-                x = Math.Min(arr[i], x);
-            }
-            int Nod = x;
-            for (; Nod > 1; Nod--)
-            {
-                if (IsModZero(arr,Nod))
-                    break;
-            }
-            return Nod;
         }
 
         private bool IsModZero(int[] arr, int num)
